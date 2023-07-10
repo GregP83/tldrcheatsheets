d7b0ddc3307b25dd69332a94137987f629f4f141
gregp <gregp83@gmail.com>
2023-07-09 22:57:59 -0700

    Add search functionality

--- cheatsheets_flask.py
+++ cheatsheets_flask.py
@@ -4,12 +4,16 @@ import sqlite3
 
 app = Flask(__name__)
 
-def get_cheatsheets():
+def get_cheatsheets(searchQuery):
     res = ""
     conn = sqlite3.connect('static/cheatsheets.db') 
     cursor = conn.cursor()
-    query = "SELECT angle, title, content FROM cheatsheets"  
-    cursor.execute(query)
+    query = "SELECT angle, title, content FROM cheatsheets"
+    values = ()
+    if searchQuery:
+        query += " WHERE title LIKE ? OR content LIKE ?"
+        values = ('%' + searchQuery + '%', '%' + searchQuery + '%')
+    cursor.execute(query, values)
     rows = cursor.fetchall()
     for row in rows:
         angle, title, content = row[0], row[1], row[2]
@@ -29,13 +33,16 @@ def add_cheatsheet(title, content):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    search = None
+    if request.method == 'GET':
+        search = request.args.get('search')
     if request.method == 'POST':
         title = request.form.get('title')
         content = request.form.get('content')
         if title and content:
             add_cheatsheet(title, content)
         
-    cheatsheets_html = get_cheatsheets()
+    cheatsheets_html = get_cheatsheets(search)
     return render_template("cheatsheets_index.html", content=cheatsheets_html)
 
 app.run(host="0.0.0.0", port=8080)
